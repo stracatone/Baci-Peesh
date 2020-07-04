faafd10a943ae29fff75eb58137def7504078ff4
Bojan Negovanovic <b.negovanovic@levi9.com>
2020-07-04 12:45:14 +0200

    Added db

---
+++ app/src/main/java/com/wordgenerator/app/model/Word.kt
@@ -0,0 +1,3 @@
+package com.wordgenerator.app.model
+
+class Word(val id: Int, val name: String, val meaning: String)
--- app/src/main/java/com/wordgenerator/app/ui/MainActivity.kt
+++ app/src/main/java/com/wordgenerator/app/ui/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import com.wordgenerator.app.R
+import io.paperdb.Paper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // init database
+        Paper.init(applicationContext)
 
         // connect bottom navigation with navigation controller for this flow
         val navController = Navigation.findNavController(this, R.id.tabsNavigationHostFragment)
